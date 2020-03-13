1ec7c52534c071ded4921948816b273519e06cfa
Meonaar <48944686+Meonaar@users.noreply.github.com>
2020-03-13 13:43:29 +0300

    Alpha

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] Objects;
+    void Start()
+    {
+        NewObject();
+    }
+
+    // Update is called once per frame
+
+    public void NewObject()
+    {
+        Instantiate(Objects[Random.Range(0, Objects.Length)], transform.position, Quaternion.identity);
+    }
+}
